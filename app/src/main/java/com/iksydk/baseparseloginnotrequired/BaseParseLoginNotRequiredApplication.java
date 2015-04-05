c3dd542f2881578fa59dcafede446997df13d99a
Billy Laney <billy3@gmail.com>
2015-04-05 17:09:22 -0500

    Device installation details saved to parse, can push messaging now

---
+++ app/src/main/java/com/iksydk/baseparseloginnotrequired/BaseParseLoginNotRequiredApplication.java
@@ -0,0 +1,44 @@
+package com.iksydk.baseparseloginnotrequired;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseInstallation;
+import com.parse.PushService;
+import com.parse.SaveCallback;
+
+/**
+ * Created by Billy on 4/5/2015.
+ */
+public class BaseParseLoginNotRequiredApplication extends Application
+{
+    private static final String TAG = BaseParseLoginNotRequiredApplication.class.getSimpleName();
+
+    @Override public void onCreate()
+    {
+        super.onCreate();
+
+        Parse.initialize(this, "kbNRJSdaS9Fu6dus85lse4phAhFRMOjQlNvTKLkp", "We7TXsvydgCOZzWvYoFi3qb8eueOCvMPGANcQBii");
+
+        ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+
+        installation.saveInBackground(new SaveCallback()
+        {
+            @Override
+            public void done(ParseException e)
+            {
+                if(e == null)
+                {
+                    PushService.setDefaultPushCallback(BaseParseLoginNotRequiredApplication.this, MainActivity.class);
+                    Log.e(TAG, "Parse registration success (launch)");
+                }
+                else
+                {
+                    Log.e(TAG, "Parse registration failed (launch): " + e.getMessage());
+                }
+            }
+        });
+    }
+}
